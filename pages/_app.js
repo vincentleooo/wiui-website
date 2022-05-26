a9d2cba3b4c345a6995af27e8ec208bdadc0d51d
Vincent Leonardo <vincentleoooo@icloud.com>
2022-05-26 19:44:49 +0800

    feat: componentize all things and add mdxprovider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MDXProvider } from "@mdx-js/react";
+import styles from "../styles/Articles.module.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="shortBody">
+      <Header />
+      <div className="mainDiv">
+        <MDXProvider
+          components={{
+            p: (props) => <p {...props} className={styles.para} />,
+            h1: (props) => <h1 {...props} className={styles.h1} />,
+          }}
+        >
+          <Component {...pageProps} />
+        </MDXProvider>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
