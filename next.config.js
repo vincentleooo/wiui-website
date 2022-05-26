099928002d311231944ebe42de529f9212f29b8d
Vincent Leonardo <vincentleoooo@icloud.com>
2022-05-26 19:44:18 +0800

    add: configs for mdx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const withPlugins = require("next-compose-plugins");
 const nextConfig = {
   reactStrictMode: true,
 }
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    providerImportSource: "@mdx-js/react",
+  },
+})
 
-module.exports = nextConfig
+module.exports = withPlugins(
+  [
+    [withMDX, {pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],}]
+  ],
+  nextConfig
+)
