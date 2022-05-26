9d9bedc420751d052086e2e3ebfa575a41f5f2f1
Vincent Leonardo <vincentleoooo@icloud.com>
2022-05-27 00:32:50 +0800

    add: custom mdxproviders and links

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,23 @@ import { MDXProvider } from "@mdx-js/react";
 import styles from "../styles/Articles.module.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Link from "next/link"
+
+// Taken from https://github.com/leerob/leerob.io/blob/main/components/MDXComponents.tsx#L13
+const CustomLink = (props) => {
+  const href = props.href;
+  const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'));
+
+  if (isInternalLink) {
+    return (
+      <Link href={href}>
+        <a {...props} className={styles.link}>{props.children}</a>
+      </Link>
+    );
+  }
+
+  return <a target="_blank" rel="noopener noreferrer" className={styles.link} {...props} />;
+};
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -13,6 +30,10 @@ function MyApp({ Component, pageProps }) {
           components={{
             p: (props) => <p {...props} className={styles.para} />,
             h1: (props) => <h1 {...props} className={styles.h1} />,
+            h2: (props) => <h2 {...props} className={styles.h2} />,
+            a: CustomLink,
+            ul: (props) => <ul {...props} className={styles.ul} />,
+            ol: (props) => <ol {...props} className={styles.ol} />,
           }}
         >
           <Component {...pageProps} />
