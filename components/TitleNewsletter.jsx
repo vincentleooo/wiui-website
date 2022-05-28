c529bbf19efcd0f487896e436c5a2a76f5ce0671
Vincent Leonardo <vincentleoooo@icloud.com>
2022-05-29 00:28:01 +0800

    fix: custom emoji img

--- components/TitleNewsletter.jsx
+++ components/TitleNewsletter.jsx
@@ -1,10 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
 import styles from "./Title.module.css";
-import Image from "next/image"
 
 export default function TitleNewsletter(props) {
   return (
     <div>
-      <h1 className={styles.title}>{props.title}&#8288;<span style={{fontSize: "1.2rem"}}>&nbsp;</span><Image style={{display: "inline-block"}} src="/images/wiuithepeople.png" alt="WIUI emoji" className={styles.image} height={38} width={12}></Image></h1>
+      <h1 className={styles.title}>{props.title}&#8288;<span style={{fontSize: "1.2rem"}}>&nbsp;</span><img style={{display: "inline-block"}} src="/images/wiuithepeople.png" alt="WIUI emoji" className={styles.image} ></img></h1>
       <p className={styles.caption}>{props.caption ? props.caption : ""}</p>
     </div>
   );
