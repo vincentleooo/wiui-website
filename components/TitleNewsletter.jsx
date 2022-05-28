21f62bbf145b011133f0af9887e9451d578e1f9c
Vincent Leonardo <vincentleoooo@icloud.com>
2022-05-28 08:31:02 +0800

    fix: emoji img line break prevented

--- components/TitleNewsletter.jsx
+++ components/TitleNewsletter.jsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function TitleNewsletter(props) {
   return (
     <div>
-      <h1 className={styles.title}>{props.title}<span style={{fontSize: "1.2rem"}}>&nbsp;</span><Image src="/images/wiuithepeople.png" className={styles.image} height={38} width={12}></Image></h1>
+      <h1 className={styles.title}>{props.title}&#8288;<span style={{fontSize: "1.2rem"}}>&nbsp;</span><Image style={{display: "inline-block"}} src="/images/wiuithepeople.png" alt="WIUI emoji" className={styles.image} height={38} width={12}></Image></h1>
       <p className={styles.caption}>{props.caption ? props.caption : ""}</p>
     </div>
   );
