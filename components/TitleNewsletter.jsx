2fdfb55e20dc9dd099944d68a954a726a7db39bc
Vincent Leonardo <vincentleoooo@icloud.com>
2022-05-30 00:10:46 +0800

    fix: newsletter title

--- components/TitleNewsletter.jsx
+++ components/TitleNewsletter.jsx
@@ -4,7 +4,7 @@ import styles from "./Title.module.css";
 export default function TitleNewsletter(props) {
   return (
     <div>
-      <h1 className={styles.title}>{props.title}&#8201;<img style={{display: "inline-block"}} src="/images/wiuithepeople.png" alt="WIUI emoji" className={styles.image} ></img></h1>
+      <h1 className={styles.title}>WIUI The <span style={{whiteSpace: "nowrap"}}>People<div style={{width: "0.3rem", display: "inline-block"}}></div><img style={{display: "inline-block"}} src="/images/wiuithepeople.png" alt="WIUI emoji" className={styles.image} ></img></span></h1>
       <p className={styles.caption}>{props.caption ? props.caption : ""}</p>
     </div>
   );
