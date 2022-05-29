aa5fc0ef636493a45214934e00491772cb469f01
Vincent Leonardo <vincentleoooo@icloud.com>
2022-05-30 00:01:23 +0800

    fix: newsletter title spacing?

--- components/TitleNewsletter.jsx
+++ components/TitleNewsletter.jsx
@@ -4,7 +4,7 @@ import styles from "./Title.module.css";
 export default function TitleNewsletter(props) {
   return (
     <div>
-      <h1 className={styles.title}>{props.title}&#8197;<img style={{display: "inline-block"}} src="/images/wiuithepeople.png" alt="WIUI emoji" className={styles.image} ></img></h1>
+      <h1 className={styles.title}>{props.title}&#8201;<img style={{display: "inline-block"}} src="/images/wiuithepeople.png" alt="WIUI emoji" className={styles.image} ></img></h1>
       <p className={styles.caption}>{props.caption ? props.caption : ""}</p>
     </div>
   );
