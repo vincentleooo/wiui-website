eb94c831773e7511cf5f055fe02825861f0559e3
Vincent Leonardo <vincentleoooo@icloud.com>
2022-05-30 21:01:24 +0800

    fix: clean code

--- pages/_app.js
+++ pages/_app.js
@@ -3,22 +3,31 @@ import { MDXProvider } from "@mdx-js/react";
 import styles from "../styles/Articles.module.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Link from "next/link"
+import Link from "next/link";
 
 // Taken from https://github.com/leerob/leerob.io/blob/main/components/MDXComponents.tsx#L13
 const CustomLink = (props) => {
   const href = props.href;
-  const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'));
+  const isInternalLink = href && (href.startsWith("/") || href.startsWith("#"));
 
   if (isInternalLink) {
     return (
       <Link href={href}>
-        <a {...props} className={styles.link}>{props.children}</a>
+        <a {...props} className={styles.link}>
+          {props.children}
+        </a>
       </Link>
     );
   }
 
-  return <a target="_blank" rel="noopener noreferrer" className={styles.link} {...props} />;
+  return (
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      className={styles.link}
+      {...props}
+    />
+  );
 };
 
 function MyApp({ Component, pageProps }) {
